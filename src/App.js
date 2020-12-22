485c8ed4d93d3804d677f00f8593e6a48faa2d76
sahuvaibhav1999 <vaibhavsahu1999@gmail.com>
2020-12-22 11:41:02 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import JSONDATA from './csvjson.json'
+import {useState} from 'react'
+//ifsc,bank_id,branch,address,city,district,state,bank_name
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="bank-branches">Bank Branches</h1>
+      <div>  
+        <select id="sel" onChange={event => {setSearchTerm(event.target.value)}}>  
+          <option value = "banglore">Banglore </option>  
+          <option value = "mumbai"> Mumbai </option>  
+          <option value = "pune"> Pune </option>  
+          <option value = "delhi"> Delhi </option>  
+          <option value = "chennai"> Chennai </option>  
+
+
+        </select>  
+
+        <input id="search-bar" type="text" placeholder="Search" onChange={event => {setSearchTerm(event.target.value)}}/>
+        {JSONDATA.filter((val)=>{
+          if (searchTerm === "") {
+            return val
+          } else if (val.ifsc.toLowerCase().includes(searchTerm.toLowerCase()) || val.bank_id.toString().toLowerCase().includes(searchTerm.toLowerCase()) 
+            || val.branch.toLowerCase().includes(searchTerm.toLowerCase()) || val.address.toLowerCase().includes(searchTerm.toLowerCase())
+            || val.city.toLowerCase().includes(searchTerm.toLowerCase()) || val.district.toLowerCase().includes(searchTerm.toLowerCase())
+            || val.state.toLowerCase().includes(searchTerm.toLowerCase()) || val.bank_name.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return val
+          }
+          else if (val.city.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val
+          }
+        }).map((val, key) => {
+          return (
+            <div>
+              <table class="t1">
+                <tr>
+                  <th>{val.ifsc}</th>
+                  <th>{val.bank_id}</th>
+                  <th>{val.branch}</th>
+                  <th>{val.address}</th>
+                  <th>{val.city}</th>
+                  <th>{val.district}</th>
+                  <th>{val.state}</th>
+                  <th>{val.bank_name}</th>
+                </tr>
+              </table>
+              
+            </div>
+          )
+        })}
+      </div>
+      
+      <div class="divt"> 
+      <table class="table1">
+      <tr>
+        <th>IFSC</th>
+        <th>BANK_ID</th> 
+        <th>BRANCH</th>
+        <th>ADDRESS</th>
+        <th>CITY</th> 
+        <th>DISTRICT</th>
+        <th>STATE</th> 
+        <th>BANK NAME</th>
+      </tr>
+      
+      </table>
+      </div>
     </div>
   );
 }
